0130e34386e697b735c2ce3bb4b070788fe4634d
arkkad3 <roy.balmera1@gmail.com>
2026-04-16 17:38:32 +0800

    Update vpn2.ps1

--- vpn2.ps1
+++ vpn2.ps1
@@ -1,1 +1,10 @@
+$url = "https://github.com/username/repo/releases/download/v1.0/installer.exe"
+$output = "$env:TEMP\installer.exe"
 
+Write-Host "Downloading installer..."
+Invoke-WebRequest $url -OutFile $output
+
+Write-Host "Installing..."
+Start-Process $output -ArgumentList "/silent" -Wait
+
+Write-Host "Done!"
