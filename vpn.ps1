ddced5604d7d4ca2307ad93b057299990043178b
arkkad3 <roy.balmera1@gmail.com>
2026-04-16 17:32:14 +0800

    Update vpn.ps1

--- vpn.ps1
+++ vpn.ps1
@@ -1,1 +1,8 @@
+$url = "https://raw.githubusercontent.com/username/repo/main/installer.exe"
+$output = "$env:TEMP\installer.exe"
 
+# Download the EXE
+Invoke-WebRequest $url -OutFile $output
+
+# Run the installer
+Start-Process $output
