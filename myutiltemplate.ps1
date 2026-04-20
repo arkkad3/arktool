daa962ba93420ce0dddf70972964461185edd168
arkkad3 <roy.balmera1@gmail.com>
2026-04-20 17:59:56 +0800

    Update myutiltemplate.ps1

--- myutiltemplate.ps1
+++ myutiltemplate.ps1
@@ -115,9 +115,7 @@ $inputXML = @'
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
         Title="WinUtil Template" Height="700" Width="1100">
   <Grid>
-    <TextBlock Text="Replace this XAML with your existing WinUtil UI (unchanged)." 
-               VerticalAlignment="Center" HorizontalAlignment="Center"/>
-               <Button Content="Hi" HorizontalAlignment="Left" Margin="118,127,0,0" VerticalAlignment="Top" Height="31" Width="176"/>
+    <Button x:Name="hiBtn" Content="Hi" HorizontalAlignment="Left" Margin="118,127,0,0" VerticalAlignment="Top" Height="31" Width="176" Click="hiBtn_Click"/>
   </Grid>
 </Window>
 '@
